ce2d660595690f4092770d61719e0e6d2103d83b
Glasck <glasckint@gmail.com>
2025-06-20 19:18:31 +0200

    init

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,10 +7,9 @@ export default async function Home() {
     <div className="flex flex-col min-h-screen w-full">
       <Header />
       <h1>Hello</h1>
-      <h2>{t("Description")}</h2>
-      <h3>{t("Description")}</h3>
-      <p>{t("Description")}</p>
-      <div className="subtilte">{t("Description")}</div>
+
+      <p>{t("Subtitle5")}</p>
+
     </div>
   );
 }
